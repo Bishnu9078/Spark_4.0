dcc9d2cfad5bbb41705ace75173745c5ff82f6ab
bishnu <bishnuprasadsahoo068@gmail.com>
2025-11-26 22:11:50 +0530

    Issues in Multhreading

--- src/Multithreading/Problem.java
+++ src/Multithreading/Problem.java
@@ -1,5 +1,16 @@
 package Multithreading;
 
-class 
+class MyThread3  {
+
+    int sum=0;
+    public void add(int value){
+        sum= sum +value;
+    }
+
+}
 public class Problem {
+    public static void main(String[] args) {
+
+        
+    }
 }
