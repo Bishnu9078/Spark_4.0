8b7bcc46d6ad997d9cae5b880933b98d6c32e778
bishnu <bishnuprasadsahoo068@gmail.com>
2025-11-28 17:28:47 +0530

    JDBC

--- src/Multithreading/MainThread.java
+++
@@ -1,8 +0,0 @@
-package Multithreading;
-
-public class MainThread {
-    public static void main(String[] args) {
-        System.out.println("Thread Name "+Thread.currentThread().getName());
-
-    }
-}
