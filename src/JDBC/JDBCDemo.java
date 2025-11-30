1652f7f588eca540237dccc604cfe79dc43e101b
bishnu <bishnuprasadsahoo068@gmail.com>
2025-11-30 18:33:20 +0530

    Demo in JDBC

---
+++ src/JDBC/JDBCDemo.java
@@ -0,0 +1,32 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCDemo {
+
+    public static void main(String[] args)
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mystd", "root", "Bishnu@068");
+
+            Statement statement = con.createStatement();
+            String query = "select * from student where id = 3";
+
+            ResultSet rs = statement.executeQuery(query);
+            System.out.println("--------------Read data---------------");
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id")+ " | "+
+                                rs.getString("stdName")+ " | "+
+                                rs.getInt("age")
+                );
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
